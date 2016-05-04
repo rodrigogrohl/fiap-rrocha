bb849680a19ba07beaa55a564757f2ac5691b4ff
rm48236 <rm48236@L2901MICRO09.fiap.com.br>
2016-05-04 20:45:37 -0300

    Aula 01

---
+++ Persistencia/src/main/java/br/com/rrocha/fiap/Persistencia/aula01/entity/Cliente.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package br.com.rrocha.fiap.Persistencia.aula01.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author rm48236
+ *
+ */
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 5686526896040723845L;
+
+	private int id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private List<Pedido> pedidos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public Cliente() { }
+}
